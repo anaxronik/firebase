f0efc87756622a6e282f57d4d0d3d612c3f0fda6
Sergey Komarov <anaxronik@gmail.com>
2022-07-17 22:37:01 +0500

    create some pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,8 +1,13 @@
 import MainPage from "../pages/MainPage";
+import UsersPage from "../pages/UsersPage";
+import { URL_BASE, URL_USERS } from "./urls";
 
 export type IRoute = {
   path: string;
   component: () => JSX.Element;
 };
 
-export const routes: IRoute[] = [{ path: "/", component: MainPage }];
+export const routes: IRoute[] = [
+  { path: URL_BASE, component: MainPage },
+  { path: URL_USERS, component: UsersPage },
+];
---
+++ src/router/urls.ts
@@ -0,0 +1,2 @@
+export const URL_BASE: string = "/";
+export const URL_USERS: string = "/users";
